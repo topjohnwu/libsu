7b1f56e2dbbd8eb4ffe4c1087d2caf20004a2d04
Victor Lapin <victor@slimroms.org>
2018-07-30 14:36:41 +0300

    Add wrapper functions for Single, Observable and Completable

---
+++ rxjava2/src/main/java/com/github/topjohnwu/libsu/rxjava2/RxShell.kt
@@ -0,0 +1,36 @@
+package com.github.topjohnwu.libsu.rxjava2
+
+import com.topjohnwu.superuser.CallbackList
+import com.topjohnwu.superuser.Shell
+import io.reactivex.Completable
+import io.reactivex.Observable
+import io.reactivex.Single
+
+object RxShell {
+    @JvmStatic
+    fun Shell.Job.asCompletable(): Completable = Completable.create { emitter ->
+        this.submit {
+            emitter.onComplete()
+        }
+    }
+
+    @JvmStatic
+    fun Shell.Job.asObservable(): Observable<String> = Observable.create { emitter ->
+        val callback = object : CallbackList<String>() {
+            override fun onAddElement(e: String) {
+                emitter.onNext(e)
+            }
+        }
+        this.to(callback)
+                .submit {
+                    emitter.onComplete()
+                }
+    }
+
+    @JvmStatic
+    fun Shell.Job.asSingle(): Single<Shell.Result> = Single.create { emitter ->
+        this.submit {
+            emitter.onSuccess(it)
+        }
+    }
+}
