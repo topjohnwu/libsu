99c8a430bceefcd0e6904bdb48ab1168d9a3fc1d
topjohnwu <topjohnwu@gmail.com>
2019-01-04 05:02:12 +0800

    Update BusyBox

--- busybox/src/main/java/com/topjohnwu/superuser/BusyBoxInstaller.java
+++ busybox/src/main/java/com/topjohnwu/superuser/BusyBoxInstaller.java
@@ -27,38 +27,45 @@ import androidx.annotation.NonNull;
  * common shell utilities. Some operations in {@link com.topjohnwu.superuser.io} depends on a
  * busybox to work properly, check before using them.
  * <p>
- * Note: the busybox binaries will add around 1.42MB to your APK.
+ * Note: the busybox binaries will add around 1.51MB to your APK.
  */
 public class BusyBoxInstaller extends Shell.Initializer {
 
-    private static final String ARM_MD5 = "482cb744785ad638cf68931164853c71";
-    private static final String X86_MD5 = "db75965c82d90b45777cbd6d114f6b47";
-    private static final int APPLET_NUM = 339;
+    private static final String ARM_MD5 = "322eacc36d95d17dafba0d4cefe9c73c";
+    private static final String X86_MD5 = "7101310d3826f14f90dd0e5e2ae5e52c";
+    private static final int APPLET_NUM = 323;
 
     @Override
     public boolean onInit(Context context, @NonNull Shell shell) {
         Context de = Build.VERSION.SDK_INT >= Build.VERSION_CODES.N
                 ? context.createDeviceProtectedStorageContext() : context;
         File bbPath = new File(de.getFilesDir().getParentFile(), "busybox");
-        bbPath.mkdirs();
         File bb = new File(bbPath, "busybox");
         // Get architecture
         List<String> archs = Arrays.asList(Build.CPU_ABI);
         boolean x86 = archs.contains("x86");
         if (!bb.exists() || !ShellUtils.checkSum("MD5", bb, x86 ? X86_MD5 : ARM_MD5)) {
-            for (File f : bbPath.listFiles()) {
+            bbPath.mkdirs();
+            for (File f : bbPath.listFiles())
                 f.delete();
-            }
-            try (InputStream in = de.getResources().openRawResource(x86 ? R.raw.busybox_x86 : R.raw.busybox_arm);
+            try (InputStream in = de.getResources().openRawResource(
+                    x86 ? R.raw.busybox_x86 : R.raw.busybox_arm);
                  OutputStream out = new FileOutputStream(bb)) {
                 ShellUtils.pump(in, out);
             } catch (IOException e) {
                 InternalUtils.stackTrace(e);
+                return true;
             }
-            bb.setExecutable(true);
         }
         if (bbPath.listFiles().length != APPLET_NUM + 1) {
-            shell.newJob().add(bbPath + "/busybox --install -s " + bbPath).exec();
+            try {
+                bb.setExecutable(true);
+                Runtime.getRuntime().exec(new String[] {
+                        bb.getPath(), "--install", "-s", bbPath.getPath() }).waitFor();
+            } catch (InterruptedException | IOException e) {
+                InternalUtils.stackTrace(e);
+                return true;
+            }
         }
         shell.newJob().add("export PATH=" + bbPath + ":$PATH").exec();
         return true;
