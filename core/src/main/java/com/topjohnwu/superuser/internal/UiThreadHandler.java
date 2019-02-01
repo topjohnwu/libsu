d99f481563f3c10cd45af2abbbf001a03f7732a7
topjohnwu <topjohnwu@gmail.com>
2019-01-31 23:45:08 -0500

    Make main handler in UiThreadHandler public

--- core/src/main/java/com/topjohnwu/superuser/internal/UiThreadHandler.java
+++ core/src/main/java/com/topjohnwu/superuser/internal/UiThreadHandler.java
@@ -22,7 +22,7 @@ import android.os.Looper;
 import com.topjohnwu.superuser.ShellUtils;
 
 public class UiThreadHandler {
-    private static Handler handler = new Handler(Looper.getMainLooper());
+    public static Handler handler = new Handler(Looper.getMainLooper());
     public static void run(Runnable r) {
         if (ShellUtils.onMainThread()) {
             r.run();
