e73cbfb3b3943532b32a8a922074a5c9b9176ad3
topjohnwu <topjohnwu@gmail.com>
2020-07-26 05:52:15 -0700

    Properly format test.cpp

--- example/src/main/cpp/test.cpp
+++ example/src/main/cpp/test.cpp
@@ -1,12 +1,24 @@
-//
-// Created by John Wu on 7/26/20.
-//
+/*
+ * Copyright 2020 John "topjohnwu" Wu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include <jni.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <string>
 
 extern "C" JNIEXPORT JNICALL
 jint Java_com_topjohnwu_libsuexample_MainActivity_00024ExampleService_nativeGetUid(
