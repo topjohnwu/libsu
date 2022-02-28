38362fc431d7db02350b7263a61146ee6044438d
topjohnwu <topjohnwu@gmail.com>
2022-02-27 22:18:27 -0800

    Update BusyBox

--- busybox/src/main/java/com/topjohnwu/superuser/BusyBoxInstaller.java
+++ busybox/src/main/java/com/topjohnwu/superuser/BusyBoxInstaller.java
@@ -16,6 +16,8 @@
 
 package com.topjohnwu.superuser;
 
+import static com.topjohnwu.superuser.ShellUtils.fastCmdResult;
+
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Build;
@@ -29,8 +31,6 @@ import java.io.File;
 import java.lang.reflect.Method;
 import java.util.UUID;
 
-import static com.topjohnwu.superuser.ShellUtils.fastCmdResult;
-
 /**
  * An initializer that installs and setup the bundled BusyBox.
  * <p>
@@ -42,12 +42,10 @@ import static com.topjohnwu.superuser.ShellUtils.fastCmdResult;
  * Register this class with {@link Shell.Builder#setInitializers(Class[])} to let {@code libsu}
  * install and setup the shell to use the bundled BusyBox binary.
  * <p>
- * After the initializer is run, the shell will be using BusyBox's "Standalone Mode ASH".
- * In this state, all commands will <b>always call the one in BusyBox regardless of PATH</b>.
- * To specifically call a command elsewhere, use the full path (e.g. {@code /system/bin/ls -l}).
- * This makes sure all commands are using the applets from BusyBox, providing predictable
- * behavior so that developers can have less headache handling different implementation of the
- * common shell utilities.
+ * After the initializer is run, the shell will be using BusyBox's "Standalone Mode ASH" mode.
+ * Please refer to the
+ * <a href="https://topjohnwu.github.io/Magisk/guides.html#busybox">Magisk Documentation</a>
+ * for more details on how "Standalone Mode ASH" mode works.
  */
 public class BusyBoxInstaller extends Shell.Initializer {
 
@@ -73,7 +71,7 @@ public class BusyBoxInstaller extends Shell.Initializer {
         job.add("export ASH_STANDALONE=1");
 
         File lib = new File(context.getApplicationInfo().nativeLibraryDir, "libbusybox.so");
-        if (shell.isRoot() && !fastCmdResult(shell, lib + " sh -c \"" + lib + " true\"")) {
+        if (shell.isRoot() && !fastCmdResult(shell, lib + " sh -c '" + lib + " true'")) {
             // This happens ONLY on some older Samsung devices
             if (fastCmdResult(shell, "[ -x $(magisk --path)/busybox/busybox ]")) {
                 // Modern Magisk installed, use that instead
