ea1858758f1f1121d2448e938839d266cbcf7b34
topjohnwu <topjohnwu@gmail.com>
2022-05-02 21:46:00 -0700

    Publish libsu 5.0.0 javadoc

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.topjohnwu.superuser"},{"l":"com.topjohnwu.superuser.io"},{"l":"com.topjohnwu.superuser.ipc"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.topjohnwu.superuser"},{"l":"com.topjohnwu.superuser.io"},{"l":"com.topjohnwu.superuser.ipc"},{"l":"com.topjohnwu.superuser.nio"}]
