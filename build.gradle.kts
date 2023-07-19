ec2d4d5cd282a3fa3d765cd5fb24745c9edbed84
topjohnwu <topjohnwu@gmail.com>
2023-07-19 09:11:12 -0700

    Update AGP

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:8.0.1")
+        classpath("com.android.tools.build:gradle:8.0.2")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
@@ -138,7 +138,7 @@ subprojects {
                 }
 
                 afterEvaluate {
-                    publishing {
+                    this.publishing {
                         publications {
                             register<MavenPublication>("maven") {
                                 from(components["release"])
